ef94b3e8ab990286060f7b557215831f08e1167b
venkat reddy <149881838+venkatreddy23@users.noreply.github.com>
2025-08-19 21:46:03 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -4,10 +4,10 @@ sum=0
 temp=n
 while temp>0:
     digit=temp%10
-    sum+=digit**power
-    temp=digit
+    num=digit**power
+    sum+=num
+    temp=temp//10
 if n==sum:
     print(n,"is an armstrong number")
 else:
-
-    print(n,"is not an armstrong number")
+    print(n,"not is not an armstrong number")
